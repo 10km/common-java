b243ae6bf18d9b86e63efdf4a865041ab5092235
10km <10km0811@sohu.com>
2018-02-12 10:30:59 +0800

    解决jar中获取资源失败问题

--- common-base/src/main/java/net/gdface/utils/ClassResourceUtils.java
+++ common-base/src/main/java/net/gdface/utils/ClassResourceUtils.java
@@ -1,18 +1,20 @@
 package net.gdface.utils;
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.net.URI;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-
-import org.apache.commons.io.Charsets;
-import org.apache.commons.io.IOUtils;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * classpath resource 工具
@@ -22,59 +24,42 @@ import org.apache.commons.io.IOUtils;
 public class ClassResourceUtils {
 
 	/**
-	 * 返回{@code path}指定的文件夹下的所有资源名
-	 * @param path
-	 * @return 资源没有找到抛出{@link FileNotFoundException}异常
-	 * @throws IOException
+	 * 文件名过滤器接口
+	 * @author guyadong
+	 *
 	 */
-	public static List<String> getFiles(String path) throws IOException {
-		if(null == path){
-			throw new NullPointerException("path is null");
-		}
-
-		InputStream in = getResourceAsStream(path);
-		if(null == in){
-			throw new FileNotFoundException(path);
-		}
-		try{
-			return IOUtils.readLines(in, StandardCharsets.UTF_8);
-		}finally{
-			in.close();
-		}
-		/*List<String> filenames = new LinkedList<String>();
-		BufferedReader br = new BufferedReader(new InputStreamReader(in));
-		try {
-			String resource;	
-			while ((resource = br.readLine()) != null) {
-				filenames.add(resource);
-			}
-			return filenames;
-
-		}catch(IOException e){
-			e.printStackTrace();
-			throw e;
-		}finally{				
-			br.close();
-		}*/	
+	public static interface FileFilter{
+		/**
+		 * Tests if a specified file should be included in a file list.
+		 * @param filename
+		 * @return
+		 */
+		boolean accept(String filename);
 	}
 	/**
-	 * 参见 {@link #getFiles(String)},<br>
+	 * 参见 {@link #getResourceFileList(Class, String)},<br>
 	 * {@link IOException}封装为{@link RuntimeException},
+	 * @param clazz Class to use when getting the System classloader
 	 * @param path
 	 * @return
 	 */
-	public static List<String> getFilesUnchedked(String path){
+	public static List<String> getFilesUnchedked(Class<?> clazz, String path){
 		try {
-			return getFiles(path);
+			return getResourceFileList(ClassResourceUtils.class,path);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	public static interface FileFilter{
-		boolean accept(String filename);
-	}
-	public static List<String> getFilesUnchecked(String path,FileFilter filter){
-		List<String> list = getFilesUnchedked(path);
+	/**
+	 * 
+	 * @param clazz Class to use when getting the System classloader
+	 * @param path
+	 * @param filter A filename filter
+	 * @return
+	 * @see #getFilesUnchedked(Class, String)
+	 */
+	public static List<String> getFilesUnchecked(Class<?> clazz,String path, FileFilter filter){
+		List<String> list = getFilesUnchedked(clazz, path);
 		if(null != filter){
 			for(Iterator<String> itor = list.iterator();itor.hasNext();){
 				String file = itor.next();
@@ -85,19 +70,17 @@ public class ClassResourceUtils {
 		}
 		return list;
 	}
-	public static final boolean resourceExist(String resource){
-		return getResource(resource)!=null;
+	/**
+	 * Returns true if resource exist.
+	 * @param clazz Class to use when getting the System classloader
+	 * @param resource
+	 * @return
+	 */
+	public static boolean resourceExist(Class<?> clazz, String resource){
+		return getResource(clazz,resource)!=null;
 	}
-	private static URL getResource(String resource) {
-		String path = normalizePath(resource);
-		final URL in = Thread.currentThread().getContextClassLoader().getResource(path);
-		return in == null ? ClassResourceUtils.class.getResource(path) : in;
-	}
-	private static InputStream getResourceAsStream(String resource) {
-		String path = normalizePath(resource);
-		return getResourceAsStream(ClassResourceUtils.class,path);
-	}
-    /**
+
+	/**
      * Return a context-relative path, beginning with a "/", that represents
      * the canonical version of the specified path after ".." and "." elements
      * are resolved out.  If the specified path attempts to go outside the
@@ -107,7 +90,7 @@ public class ClassResourceUtils {
      * @param path Path to be normalized
      * @return String normalized path
      */
-    public static final String normalizePath(String path)
+    public static String normalizePath(String path)
     {
         // Normalize the slashes and add leading slash if necessary
         String normalized = path;
@@ -125,8 +108,9 @@ public class ClassResourceUtils {
         while (true)
         {
             int index = normalized.indexOf("//");
-            if (index < 0)
+            if (index < 0){
                 break;
+            }
             normalized = normalized.substring(0, index) +
             normalized.substring(index + 1);
         }
@@ -135,18 +119,22 @@ public class ClassResourceUtils {
         while (true)
         {
             int index = normalized.indexOf("%20");
-            if (index < 0)
+            if (index < 0){
                 break;
-            normalized = normalized.substring(0, index) + " " +
-            normalized.substring(index + 3);
+            }
+            normalized =new StringBuilder()
+            	.append(normalized.substring(0, index))
+            	.append(" ")
+            	.append(normalized.substring(index + 3)).toString();
         }
 
         // Resolve occurrences of "/./" in the normalized path
         while (true)
         {
             int index = normalized.indexOf("/./");
-            if (index < 0)
+            if (index < 0){
                 break;
+            }
             normalized = normalized.substring(0, index) +
             normalized.substring(index + 2);
         }
@@ -155,10 +143,13 @@ public class ClassResourceUtils {
         while (true)
         {
             int index = normalized.indexOf("/../");
-            if (index < 0)
+            if (index < 0){
                 break;
-            if (index == 0)
-                return (null);  // Trying to go outside our context
+            }
+            if (index == 0){
+            	// Trying to go outside our context
+                return (null);  
+            }
             int index2 = normalized.lastIndexOf('/', index - 1);
             normalized = normalized.substring(0, index2) +
             normalized.substring(index + 3);
@@ -168,38 +159,71 @@ public class ClassResourceUtils {
         return (normalized);
     }
     /**
+     * 
+     * Return a normalized directory path, end with "/", but not start with one 
+     * @param path Path to be normalized
+     * @return Path to be normalized
+     * @see #normalizePath(String)
+     */
+    public static String normalizeDirPath(String path){
+    	path = normalizePath(path);
+    	if (path.startsWith("/"))
+        {
+            path = path.substring(1);
+        }
+    	if(!path.endsWith("/")){
+    		path += "/";
+    	}
+    	return path;
+    }
+    /**
+     * Returns an input stream for reading the specified resource.
+     * @param clazz Class to use when getting the System classloader (used if no Thread
+     * Context classloader available or fails to get resource).
+     * @param name name of the resource
+     * @return InputStream for the resource.
+     * @see #getResource(Class, String)
+     */
+    public static InputStream getResourceAsStream(Class<?> clazz, String name)
+    {
+        URL url = getResource(clazz,name);
+        try {
+            return url != null ? url.openStream() : null;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+    /**
      * Finds a resource with the given name.  Checks the Thread Context
      * classloader, then uses the System classloader.  Should replace all
      * calls to <code>Class.getResourceAsString</code> when the resource
      * might come from a different classloader.  (e.g. a webapp).
      * @param claz Class to use when getting the System classloader (used if no Thread
      * Context classloader available or fails to get resource).
-     * @param name name of the resource
-     * @return InputStream for the resource.
+     * @param path name of the resource
+     * @return  A <tt>URL</tt> object for reading the resource, or
+     *          <tt>null</tt> if the resource could not be found or the invoker
+     *          doesn't have adequate  privileges to get the resource.
      */
-    public static InputStream getResourceAsStream(Class<?> claz, String name)
+    public static URL getResource(Class<?> claz, String path)
     {
-        InputStream result = null;
-
+        URL result = null;
+        path = normalizePath(path);
         /**
          * remove leading slash so path will work with classes in a JAR file
          */
-        while (name.startsWith("/"))
-        {
-            name = name.substring(1);
-        }
-
+        path = path.substring(1);
         ClassLoader classLoader = Thread.currentThread()
                                     .getContextClassLoader();
 
         if (classLoader == null)
         {
             classLoader = claz.getClassLoader();
-            result = classLoader.getResourceAsStream( name );
+            result = classLoader.getResource( path );
         }
         else
         {
-            result= classLoader.getResourceAsStream( name );
+            result= classLoader.getResource( path );
 
             /**
             * for compatibility with texen / ant tasks, fall back to
@@ -209,12 +233,75 @@ public class ClassResourceUtils {
             if (result == null)
             {
                 classLoader = claz.getClassLoader();
-                if (classLoader != null)
-                    result = classLoader.getResourceAsStream( name );
+                if (classLoader != null){
+                    result = classLoader.getResource( path );
+                }
             }
         }
-
         return result;
 
+    }
+    private static final FilenameFilter FILE_FILTER = new FilenameFilter(){
+		@Override
+		public boolean accept(File dir, String name) {
+			return new File(dir,name).isFile();
+		}};
+	private static final String PROTOCOL_FILE = "file";
+	private static final String PROTOCOL_JAR = "jar";
+    /**
+     * List file names for a resource folder. Not recursive.
+     * This is basically a brute-force implementation.
+     * Works for regular files and also JARs.<br>
+     * refer to: <a href="http://www.uofr.net/~greg/java/get-resource-listing.html">Java: Listing the contents of a resource directory</a>
+     * @param clazz Any java class that lives in the same place as the resources you want.
+     * @param dirPath 
+     * @return Just the name of each member item, not the full paths.
+     * @throws IOException 
+     */
+    public static List<String> getResourceFileList(Class<?> clazz, String dirPath) throws IOException {
+    	if(null == dirPath || dirPath.isEmpty()){
+    		throw new IllegalArgumentException("path must not be null or empty");
+    	}
+    	dirPath = normalizeDirPath(dirPath);
+        URL dirURL = getResource(clazz,dirPath);
+        if(null == dirURL){
+        	throw new FileNotFoundException(dirPath);
+        }
+        
+        if (PROTOCOL_FILE.equals(dirURL.getProtocol())) {
+          /* A file path: easy enough */
+          return Arrays.asList(new File(URI.create(dirURL.toString())).list(FILE_FILTER));
+        } 
+   
+        if (PROTOCOL_JAR.equals(dirURL.getProtocol())) {
+        	/* A JAR path */
+        	//strip out only the JAR file
+        	String jarPath = dirURL.getPath().substring(5, dirURL.getPath().indexOf("!")); 
+        	JarFile jar = new JarFile(URLDecoder.decode(jarPath, "UTF-8"));
+        	LinkedList<String> result = new LinkedList<String>();
+          try{
+        	//gives ALL entries in jar
+        	  Enumeration<JarEntry> entries = jar.entries(); 
+        	  while(entries.hasMoreElements()) {
+        		  JarEntry entry = entries.nextElement();
+        		// if it is a subdirectory, skip
+        		  if(!entry.isDirectory()){
+        			  String name = entry.getName();
+            		  if (name.startsWith(dirPath)) { 
+            			  //filter according to the path
+            			  String element = name.substring(dirPath.length());
+            			  int checkSubdir = element.indexOf("/");
+            			  if (checkSubdir < 0 ) {
+            				  result.add(element);
+            			  }
+            		  }
+        		  }        		  
+        	  }
+          }finally{
+        	  jar.close();
+          }
+          return result;
+        }           
+        throw new UnsupportedOperationException("Cannot list files for URL "+dirURL);
     }
 }
