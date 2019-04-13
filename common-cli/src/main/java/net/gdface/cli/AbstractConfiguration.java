ac8bd655cad889453c3e37b5a1f18c03bf9488db
10km <10km0811@sohu.com>
2019-04-13 16:36:51 +0800

    改进loadConfig方法中对于默认值的判断(用containsKey方法代替 get方法)，允许为null的默认值(在之前的逻辑中，当默认值为null时抛出异常)

--- common-cli/src/main/java/net/gdface/cli/AbstractConfiguration.java
+++ common-cli/src/main/java/net/gdface/cli/AbstractConfiguration.java
@@ -1,6 +1,6 @@
 package net.gdface.cli;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -24,7 +24,7 @@ public abstract class AbstractConfiguration extends Context implements CommonCli
 		String key;
 		Map<String, Object> defaultMap = getDefaultValueMap();
 		if (defaultMap == null){
-			defaultMap = new HashMap<String, Object>();
+			defaultMap = Collections.emptyMap();;
 		}
 		while (it.hasNext()) {
 			opt = it.next();
@@ -33,21 +33,28 @@ public abstract class AbstractConfiguration extends Context implements CommonCli
 			if(isNeedOption(key)){				
 				Object value;
 				try{
-					value=cmd.getParsedOptionValue(key);
+					if(opt.hasArg()){
+						value=cmd.getParsedOptionValue(key);						
+					}else{
+						// 不需要参数的选项视为boolean,直接返回boolean值
+						value = cmd.hasOption(key); 
+					}
 				}catch(NoClassDefFoundError e){
 					System.out.printf("key=%s %s\n",key,cmd.getOptionValue(key));	
 					throw e;
 				}
 
-				if (null == value) {
+				if (null == value) {					
 					if (opt.isRequired()) {
 						throw new IllegalArgumentException(String.format("%s or %s not define", opt.getOpt(),
 								opt.getLongOpt()));
 					} else {
-						if (null == (value = defaultMap.get(key))) {
+						// 没有定义缺省值则抛出异常,缺省值可以为null
+						if (!defaultMap.containsKey(key)) {
 							throw new IllegalArgumentException(String.format("%s or %s not default value", opt.getOpt(),
 									opt.getLongOpt()));
 						}
+						value = defaultMap.get(key);
 					}
 				}
 				this.setProperty(key, value);
