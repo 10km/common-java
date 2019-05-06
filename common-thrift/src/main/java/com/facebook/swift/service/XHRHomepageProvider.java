82a64010487927ceb5515479f798eda54267e1d0
10km <10km0811@sohu.com>
2019-05-06 14:42:38 +0800

    commit

--- common-thrift/src/main/java/com/facebook/swift/service/XHRHomepageProvider.java
+++ common-thrift/src/main/java/com/facebook/swift/service/XHRHomepageProvider.java
@@ -1,5 +1,10 @@
 package com.facebook.swift.service;
 
+/**
+ * 用于SPI(Service Provider Interface)方式返回首页数据接口
+ * @author guyadong
+ *
+ */
 public interface XHRHomepageProvider {
 	/**
 	 * @return 返回首页页面内容
